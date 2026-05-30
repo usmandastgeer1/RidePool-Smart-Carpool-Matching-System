19de54f623fa621df3861cf7050f286ea3d9c293
USMAN DASTGEER <usmandastgeer253@gmail.com>
2026-05-30 15:50:12 +0500

    Create RiderRequest class for ride requests

---
+++ RiderRequest.java
@@ -0,0 +1,36 @@
+public class RiderRequest {
+
+    int requestId;
+    String riderName;
+    String pickup;
+    String destination;
+    int seatsNeeded;
+    String status;
+
+    public RiderRequest(int requestId,
+                        String riderName,
+                        String pickup,
+                        String destination,
+                        int seatsNeeded) {
+
+        this.requestId = requestId;
+        this.riderName = riderName;
+        this.pickup = pickup;
+        this.destination = destination;
+        this.seatsNeeded = seatsNeeded;
+
+        status = "Waiting";
+    }
+
+    public void display() {
+
+        System.out.println("--------------------------------------");
+        System.out.println("Request ID: " + requestId);
+        System.out.println("Rider Name: " + riderName);
+        System.out.println("Pickup: " + pickup);
+        System.out.println("Destination: " + destination);
+        System.out.println("Seats Needed: " + seatsNeeded);
+        System.out.println("Status: " + status);
+        System.out.println("--------------------------------------");
+    }
+}
